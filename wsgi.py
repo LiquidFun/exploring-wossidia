3ad2da9271d9f0b9c7dfaee45d7a263b14cc424d
LiquidFun <brutenis@gmail.com>
2023-01-27 02:54:40 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@
 
 from pathlib import Path
 import sys
-sys.path.insert(0, str(Path(__file__).parent / '/.venv/bin/python'))
+sys.path.insert(0, str(Path(__file__).parent.absolute()))
 
 from server import app
-application = app
+application = app.server
